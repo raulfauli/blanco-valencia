45c3aba188afaf71da5e4900219cfc1d83dfc522
Raúl Faulí <raulfauli@gmail.com>
2026-02-14 11:33:02 +0100

    hit: View transition

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,26 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwindcss from '@tailwindcss/vite';
-import rehypeExternalLinks from 'rehype-external-links';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+import rehypeExternalLinks from "rehype-external-links";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://blancovalencia.com',
-  base: '/beta/',
-  outDir: './dist/beta',
+  site: "https://blancovalencia.com",
+  outDir: "./dist/beta",
   integrations: [mdx(), sitemap()],
   markdown: {
     rehypePlugins: [
       [
         rehypeExternalLinks,
         {
-          target: '_blank',
-          rel: ['noopener', 'noreferrer']
-        }
-      ]
-    ]
+          target: "_blank",
+          rel: ["noopener", "noreferrer"],
+        },
+      ],
+    ],
   },
   vite: {
     plugins: [tailwindcss()],
