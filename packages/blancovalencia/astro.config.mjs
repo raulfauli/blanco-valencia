1bcf904c5d468cdca156e06ffbec6228711af963
Raúl Faulí <raulfauli@gmail.com>
2026-02-28 13:36:32 +0100

    blanco valencia add base url

--- packages/blancovalencia/astro.config.mjs
+++ packages/blancovalencia/astro.config.mjs
@@ -8,21 +8,22 @@ import rehypeExternalLinks from "rehype-external-links";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://blancovalencia.com",
-  outDir: "./dist/beta",
-  integrations: [mdx(), sitemap()],
-  markdown: {
-    rehypePlugins: [
-      [
-        rehypeExternalLinks,
-        {
-          target: "_blank",
-          rel: ["noopener", "noreferrer"],
-        },
-      ],
-    ],
-  },
-  vite: {
-    plugins: [tailwindcss()],
-  },
+	site: "https://blancovalencia.com",
+	base: "/beta/",
+	outDir: "./dist/beta",
+	integrations: [mdx(), sitemap()],
+	markdown: {
+		rehypePlugins: [
+			[
+				rehypeExternalLinks,
+				{
+					target: "_blank",
+					rel: ["noopener", "noreferrer"],
+				},
+			],
+		],
+	},
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
